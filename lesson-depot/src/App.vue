067be6f16489926ee49cf9df0920800748e8cfc6
Maaz Chowdhry <maazchowdhry2001@gmail.com>
2024-03-05 13:46:46 +0000

    Refactor code and update main.js and index.html files

--- lesson-depot/src/App.vue
+++ lesson-depot/src/App.vue
@@ -1,47 +1,46 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+  <div id="app">
+    <header>
+      <h1>{{sitename}}</h1>
+      <div>Current view: {{ currentView }}</div>
+      <button @click="showCheckout">{{ this.cart.length }} Checkout</button>
+    </header>
+    <main>
+      <component :is="currentView"></component>
+    </main>
+  </div>
+ </template>
+ <script>
+  import ProductList from "./components/ProductList.vue";
+  import Checkout from "./components/Checkout.vue";
+  import { markRaw } from 'vue';
+  export default {
+    name: "App",
+    data() { 
+      return { 
+        sitename: "Vue.js Pet Depot",
+        cart: [],
+        currentView: markRaw(ProductList),
+      } 
+    },
+    components: { 
+      ProductList, 
+      Checkout 
+    },
+    methods: { 
+      showCheckout() {
+        console.log('showCheckout method is called');
+        console.log('currentView is: ', this.currentView);
+        console.log('ProductList is: ', ProductList);
+        console.log('Checkout is: ', Checkout);
+        if (this.currentView === ProductList) {
+          this.currentView = markRaw(Checkout);
+        } else {
+          this.currentView = markRaw(ProductList);
+        }
+        this.$forceUpdate();
+      }
+    }
+  };
+ </script>
+ 
